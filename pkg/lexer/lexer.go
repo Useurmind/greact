b56e057c85c3f0668978f3d3e81a07d3950e4442
Jochen Grün <jochen.gruen@googlemail.com>
2020-10-15 07:28:10 +0200

    Can lex simple elements

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -15,11 +15,18 @@ type Token struct {
 }
 
 var (
+	// Control tokens
+	NOTFOUND = Token{Name: "NOTFOUND"}
+	CONTINUELOOP = Token{Name: "CONTINUELOOP"}
+
+	// language tokens
 	EOF     = Token{Name: "EOF" }
 	NONE    = Token{Name: "NONE"}
 	ILLEGAL = Token{Name: "ILLEGAL" }
 	GSX_OPEN_ELEMENT  = Token{Name: "GSX_OPEN_ELEMENT", Rune: '<'}
 	GSX_CLOSE_ELEMENT  = Token{Name: "GSX_CLOSE_ELEMENT", Rune: '>'}
+	GSX_OPEN_CLOSING_ELEMENT  = Token{Name: "GSX_CLOSE_ELEMENT", Rune: '<', Value: "</"}
+	GSX_CLOSE_SELFCLOSE_ELEMENT  = Token{Name: "GSX_SELFCLOSE_ELEMENT", Rune: '/', Value: "/>"}
 	IDENT = Token{Name: "IDENT"}
 )
 
@@ -32,6 +39,10 @@ func NewIdent(value string) Token {
 type Position struct {
 	line   int
 	column int
+}
+
+func NewPosition(line int, column int) Position {
+	return Position{line: line, column: column}
 }
 
 type Lexer struct {
@@ -67,24 +78,20 @@ func (l *Lexer) Next() (Position, Token, error) {
 		case '\n':
 			l.nextLine()
 		case GSX_OPEN_ELEMENT.Rune:
-			return l.pos, GSX_OPEN_ELEMENT, nil				
+			return chain(l).
+				backup().
+				then(func() (Position, Token, error) { return l.lexGsxOpen() }).
+				do()
 		case GSX_CLOSE_ELEMENT.Rune:
 			return l.pos, GSX_CLOSE_ELEMENT, nil
 		default:
 			if unicode.IsSpace(r) {
 				continue // nothing to do here, just move on
 			} else if unicode.IsLetter(r) {
-				// backup and let lexIdent rescan the beginning of the ident
-				startPos := l.pos
-				err = l.backup()
-				if err != nil {
-					return l.pos, NONE, err
-				}
-				token, err := l.lexIdent()
-				if err != nil {
-					return l.pos, NONE, err
-				}
-				return startPos, token, nil
+				return chain(l).
+					backup().
+					then(func() (Position, Token, error) { return l.lexIdent() }).
+					do()
 			} else {
 				return l.pos, ILLEGAL, fmt.Errorf("Encountered illegal token %c", r)
 			}
@@ -92,13 +99,13 @@ func (l *Lexer) Next() (Position, Token, error) {
 	}
 }
 
-func (l *Lexer) backup() error {
+func (l *Lexer) backup() (Position, Token, error) {
 	if err := l.reader.UnreadRune(); err != nil {
-		return err
+		return l.pos, NONE, fmt.Errorf("Could not backup during lexing: %v", err)
 	}
 	
 	l.pos.column--
-	return nil
+	return l.pos, NONE, nil
 }
 
 func (l *Lexer) nextLine() {
@@ -106,28 +113,156 @@ func (l *Lexer) nextLine() {
 	l.pos.column = 0
 }
 
-func (l *Lexer) lexIdent() (Token, error) {
-	var value = ""
-	for {
-		r, _, err := l.reader.ReadRune()
+func (l *Lexer) lexGsxOpen() (Position, Token, error) {
+
+	// read the <
+	r, pos, token, err := l.readRune()
+	if err != nil {
+		return pos, token, err
+	}
+	if token == EOF {
+		return pos, token, err
+	}
+	startPos := pos
+
+	if r != GSX_OPEN_ELEMENT.Rune {
+		return l.backup()
+	}
+
+	// read possible /
+	r, pos, token, err = l.readRune()
+	if err != nil {
+		return pos, token, err
+	}
+	if token == EOF {
+		return pos, token, err
+	}
+
+	if r != '/' {
+		pos, token, err := l.backup()
 		if err != nil {
-			if err == io.EOF {
-				// at the end of the identifier
-				return NewIdent(value), nil
+			return pos, token, err
+		}
+
+		return l.pos, GSX_OPEN_ELEMENT, nil
+	}
+
+	return startPos, GSX_OPEN_CLOSING_ELEMENT, nil
+}
+
+func (l *Lexer) lexIdent() (Position, Token, error) {
+	value := ""
+	var startPos Position
+
+	return l.forEachRune(
+		func(r rune) (Position, Token, error) {
+			if startPos.line == 0 {
+				startPos = NewPosition(l.pos.line, l.pos.column)
 			}
 
-			return NONE, err
+			if unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_' || r == '-' {
+				value = value + string(r)
+			} else {
+				// scanned something not in the identifier
+				l.backup()
+				return startPos, NewIdent(value), nil
+			}
+
+			return l.pos, CONTINUELOOP, nil
+		},
+		func() (Position, Token, error) {
+			return startPos, NewIdent(value), nil
+		},
+	)
+}
+
+func (l *Lexer) readRune() (rune, Position, Token, error) {
+	r, _, err := l.reader.ReadRune()
+	if err != nil {
+		if err == io.EOF {
+			return 'a', l.pos, EOF, nil
 		}
 
-		l.pos.column++
-		// take every rune that is allowed in an identifier
-		if unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_' || r == '-' {
-			value = value + string(r)
-		} else {
-			// scanned something not in the identifier
-			l.backup()
-			return NewIdent(value), nil
+		// at this point there isn't much we can do, and the compiler
+		// should just return the raw error to the user
+		return 'a', l.pos, NONE, err
+	}
+
+	// update the column to the position of the newly read in rune
+	l.pos.column++
+
+	return r, l.pos, NONE, nil
+}
+
+func (l *Lexer) forEachRune(
+	handleRune func(r rune) (Position, Token, error),
+	handleEOF func() (Position, Token, error)) (Position, Token, error){
+
+	for {
+		r, pos, token, err := l.readRune()
+		if err != nil {
+			return pos, token, err
+		}
+
+		if token == EOF {
+			return handleEOF()
+		}
+
+		pos, token, err = handleRune(r)
+		if err != nil {
+			return pos, token, err
+		}
+		if token.Value != CONTINUELOOP.Value {
+			return pos, token, err
 		}
 	}
+}
+
+
+type LexerChain struct {
+	lexer *Lexer
+	actions []func() (Position, Token, error)
+}
+
+func chain(lexer *Lexer) *LexerChain {
+	return &LexerChain{
+		lexer: lexer,
+		actions: make([]func() (Position, Token, error), 0),
+	}
+}
+
+func (c *LexerChain) then(f func() (Position, Token, error)) *LexerChain {
+	c.actions = append(c.actions, f)
+
+	return c
+}
+
+func (c *LexerChain) do() (Position, Token, error) {
+	pos := Position{}
+	token := NONE
+	err := error(nil)
+	for _, action := range c.actions {
+		pos, token, err = action()
+		if err != nil {
+			return pos, token, err
+		}
+	}
+
+	return pos, token, err
+}
+
+func (c *LexerChain) backup() *LexerChain {
+	c.then(func() (Position, Token, error) { return c.lexer.backup() })
+
+	return c
+}
+
+func (c *LexerChain) forEachRune(
+	handleRune func(r rune) (Position, Token, error),
+	handleEOF func() (Position, Token, error)) *LexerChain {
+
+	c.then(func() (Position, Token, error) { return c.lexer.forEachRune(handleRune, handleEOF) })
+
+	return c
 }
 
--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -3,11 +3,15 @@ package lexer
 import (
 	"testing"
 )
-	
+
+type TokenWithPos struct {
+	token Token
+	pos   Position
+}
 
 func TestLexerCanParseEmptyFile(t *testing.T) {
 	lexer := NewLexerForTestFile(t, "test_data/empty.gsx")
-	expPos := Position{ column: 0, line: 1 }
+	expPos := Position{column: 0, line: 1}
 	expToken := EOF
 
 	pos, token, err := lexer.Next()
@@ -26,14 +30,87 @@ func TestLexerCanParseEmptyFile(t *testing.T) {
 
 func TestLexerCanParseSimpleFile(t *testing.T) {
 	lexer := NewLexerForTestFile(t, "test_data/simple.gsx")
-	expPos := Position{ column: 1, line: 2 }  // first line is a space line
-	expTokens := []Token { 
+	expPos := Position{column: 1, line: 2} // first line is a space line
+	expTokens := []Token{
 		GSX_OPEN_ELEMENT,
-		GSX_CLOSE_ELEMENT, 
+		GSX_CLOSE_ELEMENT,
 		NewIdent("some-identifier_123"),
 	}
 
 	for _, expToken := range expTokens {
+		pos, token, err := lexer.Next()
+		if err != nil {
+			t.Fatalf("Could not get next lexer token %v", err)
+		}
+
+		if pos.column != expPos.column || pos.line != expPos.line {
+			t.Errorf("Position should be %v but was %v", expPos, pos)
+		}
+
+		if token.Name != expToken.Name {
+			t.Errorf("Token should be %v but was %v", expToken, token)
+		}
+
+		expPos.line++
+	}
+}
+
+func TestLexerCanParseElementsFile(t *testing.T) {
+	lexer := NewLexerForTestFile(t, "test_data/elements.gsx")
+	expTokens := []TokenWithPos{
+		{
+			token: GSX_OPEN_ELEMENT,
+			pos:   NewPosition(1, 1),
+		},
+		{
+			token: NewIdent("div"),
+			pos:   NewPosition(1, 2),
+		},
+		{
+			token: GSX_CLOSE_ELEMENT,
+			pos:   NewPosition(1, 5),
+		},
+		{
+			token: GSX_OPEN_CLOSING_ELEMENT,
+			pos:   NewPosition(1, 6),
+		},
+		{
+			token: NewIdent("div"),
+			pos:   NewPosition(1, 8),
+		},
+		{
+			token: GSX_CLOSE_ELEMENT,
+			pos:   NewPosition(1, 11),
+		},
+		{
+			token: GSX_OPEN_ELEMENT,
+			pos:   NewPosition(2, 1),
+		},
+		{
+			token: NewIdent("span"),
+			pos:   NewPosition(2, 2),
+		},
+		{
+			token: GSX_CLOSE_ELEMENT,
+			pos:   NewPosition(2, 6),
+		},
+		{
+			token: GSX_OPEN_CLOSING_ELEMENT,
+			pos:   NewPosition(2, 7),
+		},
+		{
+			token: NewIdent("span"),
+			pos:   NewPosition(2, 9),
+		},
+		{
+			token: GSX_CLOSE_ELEMENT,
+			pos:   NewPosition(2, 13),
+		},
+	}
+
+	for _, expTokenWithPos := range expTokens {
+		expToken := expTokenWithPos.token
+		expPos := expTokenWithPos.pos
 		pos, token, err := lexer.Next()
 		if err != nil {
 			t.Fatalf("Could not get next lexer token %v", err)
